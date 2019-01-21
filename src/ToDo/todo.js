ad5fe2ad4248f475f794b92a1fd6956fa2ed42d3
Amit Raushan <amiraush@publicisgroupe.net>
2019-01-21 12:16:28 +0530

    filtered visibletodos based on filter

--- src/ToDo/todo.js
+++ src/ToDo/todo.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import "./todo.css";
-import { addToDo } from "../store/action";
+import { addToDo, setFilter } from "../store/action";
+import { SHOW_COMPLETED, SHOW_ACTIVE, SHOW_ALL } from "../store/constant";
 
 class ToDo extends Component {
   constructor(props) {
@@ -14,10 +15,10 @@ class ToDo extends Component {
     this.clearCompleted = this.clearCompleted.bind(this);
     this.toggleAll = this.toggleAll.bind(this);
     this.state = {
-      footerFlag: "",
+      // footerFlag: 1,
       toggleAll: false,
-      activeBtn: "all",
-      taskList: [],
+      // activeBtn: "all",
+      // taskList: [],
       toBeDone: [],
       completed: []
     };
@@ -25,7 +26,6 @@ class ToDo extends Component {
 
   addToTable(e) {
     if (e.keyCode === 13 && e.target.value) {
-      console.log(e.target.value);
       let obj = { value: e.target.value, completed: false };
       this.props.addToDo(obj);
       e.target.value = "";
@@ -50,14 +50,17 @@ class ToDo extends Component {
 
   showAll() {
     console.log("showAll");
+    this.props.setFilter(SHOW_ALL);
   }
 
   showActive() {
     console.log("showActive");
+    this.props.setFilter(SHOW_ACTIVE);
   }
 
   showCompleted() {
     console.log("showCompleted");
+    this.props.setFilter(SHOW_COMPLETED);
   }
 
   clearCompleted() {
@@ -69,6 +72,7 @@ class ToDo extends Component {
   }
 
   render() {
+    console.log(this.state);
     return (
       <section className="App-page">
         <h1>todos</h1>
@@ -80,7 +84,7 @@ class ToDo extends Component {
               onKeyDown={this.addToTable}
             />
           </header>
-          {this.state.footerFlag ? (
+          {this.props.todos.length ? (
             <section className="main">
               <input
                 type="checkbox"
@@ -91,7 +95,7 @@ class ToDo extends Component {
               />
               <label htmlFor="toggle-all" />
               <ul className="todo-list">
-                {this.state.taskList.map((item, index) => (
+                {this.props.todos.map((item, index) => (
                   <li key={index} className="view">
                     <input
                       type="checkbox"
@@ -121,14 +125,15 @@ class ToDo extends Component {
           ) : (
             ""
           )}
-          {this.state.footerFlag ? (
+          {this.props.originalList.length ? (
             <footer className="footer">
               <div className="items-left">
-                {this.state.toBeDone.length} items left
+                {this.props.originalList.filter(todo => !todo.completed).length}{" "}
+                items left
               </div>
               <button
                 className={`${
-                  this.state.activeBtn === "all" ? `active-class` : ""
+                  this.props.activeFilter === SHOW_ALL ? `active-class` : ""
                 } footer-btn`}
                 onClick={this.showAll}
               >
@@ -136,7 +141,7 @@ class ToDo extends Component {
               </button>
               <button
                 className={`${
-                  this.state.activeBtn === "active" ? `active-class` : ""
+                  this.props.activeFilter === SHOW_ACTIVE ? `active-class` : ""
                 } footer-btn`}
                 onClick={this.showActive}
               >
@@ -144,7 +149,9 @@ class ToDo extends Component {
               </button>
               <button
                 className={`${
-                  this.state.activeBtn === "completed" ? `active-class` : ""
+                  this.props.activeFilter === SHOW_COMPLETED
+                    ? `active-class`
+                    : ""
                 } footer-btn`}
                 onClick={this.showCompleted}
               >
@@ -171,14 +178,34 @@ class ToDo extends Component {
 }
 
 ToDo.propTypes = {
-  addToDo: PropTypes.func
+  addToDo: PropTypes.func,
+  todos: PropTypes.array,
+  activeFilter: PropTypes.string,
+  originalList: PropTypes.array
 };
-
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed);
+    case SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed);
+    default:
+      return todos;
+  }
+};
+const mapStateToProps = state => {
+  return {
+    originalList: state.todos,
+    todos: getVisibleTodos(state.todos, state.filter),
+    activeFilter: state.filter
+  };
+};
 const mapDispatchToProps = dispatch => ({
-  addToDo: data => dispatch(addToDo(data))
+  addToDo: data => dispatch(addToDo(data)),
+  setFilter: filter => dispatch(setFilter(filter))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(ToDo);
